587a9f3db30dc6831979ebf1e2eb0ecf8e1d9bda
Sruthi53 <85440645+Sruthi53@users.noreply.github.com>
2021-06-06 17:36:00 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+function Note() {
+  return (
+    <>
+      <div className="note">
+        <h1>Pomeranian</h1>
+        <p>
+          The Pomeranian is a breed of dog of the Spitz type that is named for
+          the Pomerania region in north-west Poland and north-east Germany in
+          Central Europe. Classed as a toy dog breed because of its small size,
+          the Pomeranian is descended from larger Spitz-type dogs, specifically
+          the German Spitz. Hypoallergenic: No Life span: 12 – 16 years Height:
+          7 to 12 inches (18 to 30 cm) Weight: 3 to 7 pounds (1.4 to 3.2 kg)
+          Temperament: Playful, Intelligent, Friendly, Extroverted, Sociable,
+          Active
+        </p>
+      </div>
+      <div className="note">
+        <h1>Chow Chow</h1>
+        <p>
+          The Chow Chow is a dog breed originally from northern China. The Chow
+          Chow is a sturdily built dog, square in profile, with a broad skull
+          and small, triangular, erect ears with rounded tips. The breed is
+          known for a very dense double coat that is either smooth or rough.
+          Life span: 9 – 15 years Breed of: Dog Height: Male: 48–56 cm, Female:
+          46–51 cm Colors: Black, Blue, Fawn, Cream, Red Temperament: Aloof,
+          Loyal, Independent, Quiet.The breed is known for a very dense double
+          coat that is either smooth.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Shih Tzu</h1>
+        <p>
+          The Shih Tzu is an Asian toy dog breed originating from Tibet. This
+          breed is well-known for their short snout and large round eyes, as
+          well as their ever growing coat, floppy ears, and short and stout
+          posture. Hypoallergenic: Yes Life span: 10 – 16 years Hanyu Pinyin:
+          xīshī quǎn Temperament: Playful, Intelligent, Affectionate, Clever,
+          Friendly. Color Black, White, Liver, Brindle, Liver & White, Light
+          Brown, Dark Brown, Black & White, Blue, Gold Mass: Female: 4 – 7.2 kg
+          (Adult), Male: 4 – 7.2 kg (Adult)
+        </p>
+      </div>
+      <div className="note">
+        <h1>Maltese dog</h1>
+        <p>
+          The Maltese is a breed of dog in the toy group. It is thought to have
+          originated in south-central Europe from dogs of spitz type. Despite
+          the name, it has no verified historic or scientific connection to the
+          island of Malta. Hypoallergenic: Yes Life span: 12 – 15 years Rank:
+          Breed Temperament: Playful, Affectionate, Intelligent, Docile,
+          Easygoing, Lively, Fearless, Responsive, Sweet-Tempered, Gentle,
+          Active Height: Male: 21–25 cm, Female: 20–23 cm Weight: Male: 3–4 kg,
+          Female: 3–4 kg
+        </p>
+      </div>
+      <div className="note">
+        <h1>Bichon Frise</h1>
+        <p>
+          A Bichon Frise is a small breed of dog of the bichon type.
+          Hypoallergenic: Yes Litter size: 1–6 Life span: 12 – 15 years Colour:
+          White Temperament: Playful, Affectionate, Gentle, Feisty, Cheerful,
+          Sensitive Weight: Male: 3–5 kg, Female: 3–5 kg They’re super playful
+          and intelligent, and even novice pet parents and apartment dwellers .
+        </p>
+      </div>
+      <div className="note">
+        <h1>Maltipoo</h1>
+        <p>
+          The Maltipoo (aka Multipoo, maltepoo) is a hybrid mix between a
+          maltese and a poodle. The Maltipoo has a playful and affectionate
+          nature. The Maltipoo coat can vary in color and waviness. The Maltipoo
+          is very adaptable to small living situations like apartments and small
+          homes and they do very well with children.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Note;
